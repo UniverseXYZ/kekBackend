5edd68d721eba84783af00ab48635db52275d166
Casian Lacatusu <casian.lacatusu@gmail.com>
2020-12-26 15:55:59 +0200

    Fix bond_users postgres view

--- migrations/00017_create_bond_users_view.go
+++ migrations/00017_create_bond_users_view.go
@@ -12,15 +12,19 @@ func init() {
 
 func UpBondUsersView(tx *sql.Tx) error {
 	_, err := tx.Exec(`
-		create view bond_users_with_balance as
-			with users as ( select distinct sender as address from bond_transfers union select distinct receiver from bond_transfers ),
-				 value_in as ( select sum(value) as val, sender as addr from bond_transfers group by sender ),
-				 value_out as ( select sum(value) as val, receiver as addr from bond_transfers group by receiver )
-			select address,
-				   coalesce(( select val from value_in where addr = u.address ), 0) -
-				   coalesce(( select val from value_out where addr = u.address ), 0) as balance
-			from users u
-			order by balance desc;
+		create or replace view bond_users_with_balance as
+		with users as ( select distinct sender as address
+						from bond_transfers
+						union
+						select distinct receiver
+						from bond_transfers ),
+			 value_out as ( select sum(value) as val, sender as addr from bond_transfers group by sender ),
+			 value_in as ( select sum(value) as val, receiver as addr from bond_transfers group by receiver )
+		select address,
+			   coalesce(( select val from value_in where addr = u.address ), 0) -
+			   coalesce(( select val from value_out where addr = u.address ), 0) as balance
+		from users u
+		order by balance desc;
 	`)
 	return err
 }
