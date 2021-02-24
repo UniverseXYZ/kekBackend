df9b7d4f97acda85537413297bc967d06b1f1f35
Bogdan Lacatusu <ionut.bogdy00@gmail.com>
2021-02-24 17:47:28 +0200

    Refactor code

--- api/handle_redeems.go
+++ api/handle_redeems.go
@@ -31,20 +31,21 @@ type juniorRedeem struct {
 func (a *API) handleSeniorRedeems(c *gin.Context) {
 	userAddress := c.Param("user")
 	var seniorBondRedeems []seniorRedeem
-	rows, err := a.db.Query(`select
-										b.sy_address,
-										b.buyer_address,
-										b.senior_bond_id,
-										b.underlying_in,
-										b.gain,
-										b.for_days,
-										r.fee,
-										r.block_timestamp 
-									from smart_yield_senior_buy as b
-									inner join smart_yield_senior_redeem as r
-										on r.senior_bond_address = b.senior_bond_address
-											   and r.senior_bond_id = b.senior_bond_id
-									where (r.owner_address = $1 or b.buyer_address = $1)`, userAddress)
+	rows, err := a.db.Query(`
+				select b.sy_address,
+					   b.buyer_address,
+					   b.senior_bond_id,
+					   b.underlying_in,
+					   b.gain,
+					   b.for_days,
+					   r.fee,
+					   r.block_timestamp
+				from smart_yield_senior_buy as b
+						 inner join smart_yield_senior_redeem as r
+									on r.senior_bond_address = b.senior_bond_address
+										and r.senior_bond_id = b.senior_bond_id
+				where (r.owner_address = $1 or b.buyer_address = $1
+				)`, userAddress)
 
 	if err != nil && err != sql.ErrNoRows {
 		Error(c, err)
@@ -73,19 +74,20 @@ func (a *API) handleSeniorRedeems(c *gin.Context) {
 func (a *API) handleJuniorRedeems(c *gin.Context) {
 	userAddress := strings.ToLower(c.Param("user"))
 	var juniorBondRedeems []juniorRedeem
-	rows, err := a.db.Query(`select 
-										b.sy_address,
-										b.buyer_address,
-										b.junior_bond_id,
-										b.tokens_in,
-										b.matures_at,
-										r.underlying_out,
-										r.block_timestamp
-									from smart_yield_junior_buy as b
-									inner join smart_yield_junior_redeem as r
-									    on r.junior_bond_address = b.junior_bond_address
-								           and r.junior_bond_id = b.junior_bond_id
-								where (r.owner_address = $1 or b.buyer_address = $1)`, userAddress)
+	rows, err := a.db.Query(`
+				select b.sy_address,
+					   b.buyer_address,
+					   b.junior_bond_id,
+					   b.tokens_in,
+					   b.matures_at,
+					   r.underlying_out,
+					   r.block_timestamp
+				from smart_yield_junior_buy as b
+						 inner join smart_yield_junior_redeem as r
+									on r.junior_bond_address = b.junior_bond_address
+										and r.junior_bond_id = b.junior_bond_id
+				where (r.owner_address = $1 or b.buyer_address = $1
+				)`, userAddress)
 
 	if err != nil && err != sql.ErrNoRows {
 		Error(c, err)
