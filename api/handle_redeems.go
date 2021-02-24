199fed3f9191283b5398241415de7b04d8f3d0cb
Bogdan Lacatusu <ionut.bogdy00@gmail.com>
2021-02-24 17:21:47 +0200

    Refactor sql query

--- api/handle_redeems.go
+++ api/handle_redeems.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -28,14 +29,14 @@ type juniorRedeem struct {
 func (a *API) handleSeniorRedeems(c *gin.Context) {
 	userAddress := c.Param("user")
 	var seniorBondRedeems []seniorRedeem
-	rows, err := a.db.Query(`select smart_yield_senior_buy.sy_address , smart_yield_senior_buy.buyer_address,smart_yield_senior_buy.senior_bond_id,smart_yield_senior_buy.underlying_in,
-									smart_yield_senior_buy.gain ,smart_yield_senior_buy.for_days,
-									smart_yield_senior_redeem.fee 
-								from smart_yield_senior_buy
-								inner join smart_yield_senior_redeem
-								    on smart_yield_senior_redeem.owner_address = smart_yield_senior_buy.buyer_address
-								           and smart_yield_senior_redeem.senior_bond_id = smart_yield_senior_buy.senior_bond_id
-								where smart_yield_senior_redeem.owner_address = $1`, userAddress)
+	rows, err := a.db.Query(`select b.sy_address , b.buyer_address,b.senior_bond_id,b.underlying_in,
+									b.gain ,b.for_days,
+									r.fee 
+								from smart_yield_senior_buy as b
+								inner join smart_yield_senior_redeem as r
+								    on r.senior_bond_address = b.senior_bond_address
+								           and r.senior_bond_id = b.senior_bond_id
+								where (r.owner_address = $1 or b.buyer_address = $1)`, userAddress)
 
 	if err != nil && err != sql.ErrNoRows {
 		Error(c, err)
@@ -62,16 +63,16 @@ func (a *API) handleSeniorRedeems(c *gin.Context) {
 }
 
 func (a *API) handleJuniorRedeems(c *gin.Context) {
-	userAddress := c.Param("user")
+	userAddress := strings.ToLower(c.Param("user"))
 	var juniorBondRedeems []juniorRedeem
-	rows, err := a.db.Query(`select smart_yield_junior_buy.sy_address,smart_yield_junior_buy.buyer_address,smart_yield_junior_buy.junior_bond_id,
-									smart_yield_junior_buy.tokens_in,smart_yield_junior_buy.matures_at,
-									smart_yield_junior_redeem.underlying_out
-									from smart_yield_junior_buy
-									inner join smart_yield_junior_redeem 
-									    on smart_yield_junior_redeem.owner_address = smart_yield_junior_buy.buyer_address
-								           and smart_yield_junior_redeem.junior_bond_id = smart_yield_junior_buy.junior_bond_id
-								where smart_yield_junior_redeem.owner_address = $1`, userAddress)
+	rows, err := a.db.Query(`select b.sy_address,b.buyer_address,b.junior_bond_id,
+									b.tokens_in,b.matures_at,
+									r.underlying_out
+									from smart_yield_junior_buy as b
+									inner join smart_yield_junior_redeem as r
+									    on r.junior_bond_address = b.junior_bond_address
+								           and r.junior_bond_id = b.junior_bond_id
+								where (r.owner_address = $1 or b.buyer_address = $1)`, userAddress)
 
 	if err != nil && err != sql.ErrNoRows {
 		Error(c, err)
