11ed85a6944533dc100bc13346f6db67a511091e
stefangeorgerobert <stefan_george_robert@yahoo.com>
2021-04-21 00:16:46 +0300

    Cleanup

--- state/state.go
+++ state/state.go
@@ -12,7 +12,6 @@ import (
 type State struct {
 	db *sql.DB
 
-	syPools           []types.SYPool
 	rewardPools       []types.SYRewardPool
 	monitoredAccounts []string
 }
@@ -30,52 +29,10 @@ func Init(db *sql.DB) error {
 }
 
 func Refresh() error {
-	err := loadAllSYPools()
-	if err != nil {
-		return errors.Wrap(err, "could not load SmartYield pools")
-	}
-
-	err = loadAllRewardPools()
-	if err != nil {
-		return errors.Wrap(err, "could not load SmartYield Reward pools")
-	}
-
-	err = loadAllAccounts()
+	err := loadAllAccounts()
 	if err != nil {
 		return errors.Wrap(err, "could not load monitored accounts ")
 	}
-
-	return nil
-}
-
-func loadAllSYPools() error {
-	rows, err := instance.db.Query(`select protocol_id, controller_address, model_address, provider_address, sy_address, oracle_address, junior_bond_address, senior_bond_address, receipt_token_address, underlying_address, underlying_symbol, underlying_decimals, start_at_block from smart_yield_pools;`)
-	if err != nil {
-		return errors.Wrap(err, "could not query database for SmartYield pools")
-	}
-
-	var pools []types.SYPool
-	for rows.Next() {
-		var p types.SYPool
-		err := rows.Scan(&p.ProtocolId, &p.ControllerAddress, &p.ModelAddress, &p.ProviderAddress, &p.SmartYieldAddress, &p.OracleAddress, &p.JuniorBondAddress, &p.SeniorBondAddress, &p.ReceiptTokenAddress, &p.UnderlyingAddress, &p.UnderlyingSymbol, &p.UnderlyingDecimals, &p.StartAtBlock)
-		if err != nil {
-			return errors.Wrap(err, "could not scan pools from database")
-		}
-
-		p.ControllerAddress = utils.NormalizeAddress(p.ControllerAddress)
-		p.ModelAddress = utils.NormalizeAddress(p.ModelAddress)
-		p.ProviderAddress = utils.NormalizeAddress(p.ProviderAddress)
-		p.SmartYieldAddress = utils.NormalizeAddress(p.SmartYieldAddress)
-		p.OracleAddress = utils.NormalizeAddress(p.OracleAddress)
-		p.JuniorBondAddress = utils.NormalizeAddress(p.JuniorBondAddress)
-		p.SeniorBondAddress = utils.NormalizeAddress(p.SeniorBondAddress)
-		p.ReceiptTokenAddress = utils.NormalizeAddress(p.ReceiptTokenAddress)
-		p.UnderlyingAddress = utils.NormalizeAddress(p.UnderlyingAddress)
-
-		pools = append(pools, p)
-	}
-
-	instance.syPools = pools
 
 	return nil
 }
