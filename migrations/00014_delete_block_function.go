0311a660620065c62f67621540ec2a6d684f63c3
stefangeorgerobert <stefan_george_robert@yahoo.com>
2021-05-10 17:14:24 +0300

    add __delete_entity

--- migrations/00014_delete_block_function.go
+++ migrations/00014_delete_block_function.go
@@ -12,6 +12,13 @@ func init() {
 
 func Up00006(tx *sql.Tx) error {
 	_, err := tx.Exec(`
+	create or replace function __delete_entity(in table_name varchar, in number bigint) returns void as
+	$body$
+	begin
+		execute format('delete from %1$s where included_in_block = %2$s;', table_name, number);
+	end;
+	$body$ language 'plpgsql';
+	
 		create or replace function delete_block(in block_number bigint, tables character varying[]) returns void as
 	$body$
 	declare
