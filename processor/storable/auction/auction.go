3e9e68ad8be75faa6910d4b1faa232a741725f89
stefangeorgerobert <stefan_george_robert@yahoo.com>
2021-06-03 01:05:45 +0300

    auction storable

--- processor/storable/auction/auction.go
+++ processor/storable/auction/auction.go
@@ -8,15 +8,15 @@ import (
 
 	web3types "github.com/alethio/web3-go/types"
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/lib/pq"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/kekDAO/kekBackend/types"
 	"github.com/kekDAO/kekBackend/utils"
 )
 
-var log = logrus.WithField("module", "storable(auction)")
+var logger = logrus.WithField("module", "storable(auction)")
 
 type Storable struct {
 	config     Config
@@ -24,14 +24,100 @@ type Storable struct {
 	auctionAbi abi.ABI
 }
 
-type StakingAction struct {
+type AuctionEvent struct {
 	UserAddress      string
 	TokenAddress     string
 	Amount           *big.Int
 	TransactionHash  string
 	TransactionIndex int64
 	LogIndex         int64
-	ActionType       ActionType
+}
+
+type LogAuctionCreated struct {
+	AuctionId         *big.Int       "json:\"auctionId\""
+	AuctionOwner      common.Address "json:\"auctionOwner\""
+	NumberOfSlots     *big.Int       "json:\"numberOfSlots\""
+	StartTime         *big.Int       "json:\"startTime\""
+	EndTime           *big.Int       "json:\"endTime\""
+	ResetTimer        *big.Int       "json:\"resetTimer\""
+	SupportsWhitelist bool           "json:\"supportsWhitelist\""
+	Time              *big.Int       "json:\"time\""
+}
+
+type LogERC721Deposit struct {
+	Depositor    common.Address "json:\"depositor\""
+	TokenAddress common.Address "json:\"tokenAddress\""
+	TokenId      *big.Int       "json:\"tokenId\""
+	AuctionId    *big.Int       "json:\"auctionId\""
+	SlotIndex    *big.Int       "json:\"slotIndex\""
+	NftSlotIndex *big.Int       "json:\"nftSlotIndex\""
+	Time         *big.Int       "json:\"time\""
+}
+
+type LogERC721Withdrawal struct {
+	Depositor    common.Address "json:\"depositor\""
+	TokenAddress common.Address "json:\"tokenAddress\""
+	TokenId      *big.Int       "json:\"tokenId\""
+	AuctionId    *big.Int       "json:\"auctionId\""
+	SlotIndex    *big.Int       "json:\"slotIndex\""
+	NftSlotIndex *big.Int       "json:\"nftSlotIndex\""
+	Time         *big.Int       "json:\"time\""
+}
+
+type LogBidSubmitted struct {
+	Sender     common.Address "json:\"sender\""
+	AuctionId  *big.Int       "json:\"auctionId\""
+	CurrentBid *big.Int       "json:\"currentBid\""
+	TotalBid   *big.Int       "json:\"totalBid\""
+	Time       *big.Int       "json:\"time\""
+}
+
+type LogBidWithdrawal struct {
+	Recipient common.Address "json:\"recipient\""
+	AuctionId *big.Int       "json:\"auctionId\""
+	Amount    *big.Int       "json:\"amount\""
+	Time      *big.Int       "json:\"time\""
+}
+
+type LogBidMatched struct {
+	AuctionId        *big.Int       "json:\"auctionId\""
+	SlotIndex        *big.Int       "json:\"slotIndex\""
+	SlotReservePrice *big.Int       "json:\"slotReservePrice\""
+	WinningBidAmount *big.Int       "json:\"winningBidAmount\""
+	Winner           common.Address "json:\"winner\""
+	Time             *big.Int       "json:\"time\""
+}
+
+type LogAuctionExtended struct {
+	AuctionId *big.Int "json:\"auctionId\""
+	EndTime   *big.Int "json:\"endTime\""
+	Time      *big.Int "json:\"time\""
+}
+
+type LogAuctionCanceled struct {
+	AuctionId *big.Int "json:\"auctionId\""
+	Time      *big.Int "json:\"time\""
+}
+
+type LogAuctionRevenueWithdrawal struct {
+	Recipient common.Address "json:\"recipient\""
+	SuctionId *big.Int       "json:\"auctionId\""
+	Smount    *big.Int       "json:\"amount\""
+	Time      *big.Int       "json:\"time\""
+}
+
+type LogERC721RewardsClaim struct {
+	Claimer   common.Address "json:\"claimer\""
+	SuctionId *big.Int       "json:\"auctionId\""
+	SlotIndex *big.Int       "json:\"slotIndex\""
+	Time      *big.Int       "json:\"time\""
+}
+
+type LogRoyaltiesWithdrawal struct {
+	Amount *big.Int       "json:\"amount\""
+	To     common.Address "json:\"to\""
+	Token  common.Address "json:\"token\""
+	Time   *big.Int       "json:\"time\""
 }
 
 func NewStorable(config Config, raw *types.RawData, auctionAbi abi.ABI) *Storable {
@@ -43,7 +129,7 @@ func NewStorable(config Config, raw *types.RawData, auctionAbi abi.ABI) *Storabl
 }
 
 func (a *Storable) ToDB(tx *sql.Tx) error {
-	var stakingActions []StakingAction
+	var auctionEvents []AuctionEvent
 
 	for _, data := range a.raw.Receipts {
 		for _, log := range data.Logs {
@@ -54,34 +140,23 @@ func (a *Storable) ToDB(tx *sql.Tx) error {
 				continue
 			}
 
-			if utils.LogIsEvent(log, a.auctionAbi, Deposit) {
-				d, err := a.decodeLog(log, Deposit)
+			if utils.LogIsEvent(log, a.auctionAbi, AuctionCreated) {
+				d, err := a.decodeLog(log, AuctionCreated)
 				if err != nil {
 					return err
 				}
 
-				d.ActionType = DEPOSIT
-				stakingActions = append(stakingActions, *d)
-			}
-
-			if utils.LogIsEvent(log, a.auctionAbi, Withdraw) {
-				w, err := a.decodeLog(log, Withdraw)
-				if err != nil {
-					return err
-				}
-
-				w.ActionType = WITHDRAW
-				stakingActions = append(stakingActions, *w)
+				auctionEvents = append(auctionEvents, *d)
 			}
 
 		}
 	}
-	if len(stakingActions) == 0 {
-		log.WithField("handler", "staking actions").Debug("no actions found")
+	if len(auctionEvents) == 0 {
+		logger.WithField("handler", "auction events").Debug("no event found")
 		return nil
 	}
 
-	err := a.storeActions(tx, stakingActions)
+	err := a.storeActions(tx, auctionEvents)
 	if err != nil {
 		return err
 	}
@@ -89,23 +164,85 @@ func (a *Storable) ToDB(tx *sql.Tx) error {
 	return nil
 }
 
-func (a Storable) decodeLog(log web3types.Log, event string) (*StakingAction, error) {
-	var d StakingAction
+func (a Storable) decodeLog(log web3types.Log, event string) (*AuctionEvent, error) {
+	var d AuctionEvent
 
 	data, err := hex.DecodeString(utils.Trim0x(log.Data))
 	if err != nil {
 		return nil, errors.Wrap(err, "could not decode log data")
 	}
 
-	var extraData = make(map[string]interface{})
-	err = a.auctionAbi.UnpackIntoMap(extraData, event, data)
-	if err != nil {
-		return nil, errors.Wrap(err, "could not unpack log data")
+	switch event {
+	case AuctionCreated:
+		var decoded LogAuctionCreated
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case ERC721Deposit:
+		var decoded LogERC721Deposit
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case ERC721Withdrawal:
+		var decoded LogERC721Withdrawal
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case BidSubmitted:
+		var decoded LogBidSubmitted
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case BidWithdrawal:
+		var decoded LogBidWithdrawal
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case BidMatched:
+		var decoded LogBidMatched
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case AuctionExtended:
+		var decoded LogAuctionExtended
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case AuctionCanceled:
+		var decoded LogAuctionCanceled
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case AuctionRevenueWithdrawal:
+		var decoded LogAuctionRevenueWithdrawal
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case ERC721RewardsClaim:
+		var decoded LogERC721RewardsClaim
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	case RoyaltiesWithdrawal:
+		var decoded LogRoyaltiesWithdrawal
+		err = a.auctionAbi.UnpackIntoInterface(&decoded, event, data)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not unpack log data")
+		}
+	default:
+		logger.Debug("Unknown auction event")
 	}
 
-	d.UserAddress = utils.Topic2Address(log.Topics[1])
-	d.TokenAddress = utils.Topic2Address(log.Topics[2])
-	d.Amount = extraData["amount"].(*big.Int)
 	d.TransactionIndex, err = strconv.ParseInt(log.TransactionIndex, 0, 64)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not convert transactionIndex from kek contract to int64")
@@ -120,8 +257,8 @@ func (a Storable) decodeLog(log web3types.Log, event string) (*StakingAction, er
 	return &d, nil
 }
 
-func (a Storable) storeActions(tx *sql.Tx, actions []StakingAction) error {
-	stmt, err := tx.Prepare(pq.CopyIn("auctions", "tx_hash", "tx_index", "log_index", "user_address", "token_address", "amount", "action_type", "block_timestamp", "included_in_block"))
+func (a Storable) storeActions(tx *sql.Tx, actions []AuctionEvent) error {
+	/*stmt, err := tx.Prepare(pq.CopyIn("auctions", "tx_hash", "tx_index", "log_index", "user_address", "token_address", "amount", "action_type", "block_timestamp", "included_in_block"))
 	if err != nil {
 		return err
 	}
@@ -151,7 +288,7 @@ func (a Storable) storeActions(tx *sql.Tx, actions []StakingAction) error {
 	err = stmt.Close()
 	if err != nil {
 		return err
-	}
+	}*/
 
 	return nil
 }
--- processor/storable/auction/const.go
+++ processor/storable/auction/const.go
@@ -1,11 +1,13 @@
 package auction
 
-const Deposit = "Deposit"
-const Withdraw = "Withdraw"
-
-type ActionType string
-
-const (
-	DEPOSIT  ActionType = "DEPOSIT"
-	WITHDRAW ActionType = "WITHDRAW"
-)
+const AuctionCreated = "LogAuctionCreated"
+const ERC721Deposit = "LogERC721Deposit"
+const ERC721Withdrawal = "LogERC721Withdrawal"
+const BidSubmitted = "LogBidSubmitted"
+const BidWithdrawal = "LogBidWithdrawal"
+const BidMatched = "LogBidMatched"
+const AuctionExtended = "LogAuctionExtended"
+const AuctionCanceled = "LogAuctionCanceled"
+const AuctionRevenueWithdrawal = "LogAuctionRevenueWithdrawal"
+const ERC721RewardsClaim = "LogERC721RewardsClaim"
+const RoyaltiesWithdrawal = "LogRoyaltiesWithdrawal"
